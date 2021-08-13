3756b41a1954cc2629ac67edf4422d0f9fcd32b9
snoopy95 <joebart95@free.fr>
2021-08-13 15:58:36 +0200

    Update

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,9 @@
+
 setTimeout(() => {
-    var alertNode = document.querySelector('.alert')
-    var alert = bootstrap.Alert.getInstance(alertNode)
-    alert.close()
+    var alertList = document.querySelectorAll('.alert')
+    alertList.forEach(function (alert) {
+        new bootstrap.Alert(alert).close()
+    })
 }, 5000);
 
 const fileplan = document.querySelector('.inputPlan')
@@ -10,4 +12,4 @@ if (fileplan) { fileplan.addEventListener('change', addname) }
 function addname() {
     newtext = fileplan.value.split('\\')
     document.querySelector('.labelplan').textContent = newtext[newtext.length - 1]
-}
+} ''
