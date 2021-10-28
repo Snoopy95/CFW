d9b9b450449c71502719193d1ce30d0d628f7f1c
Snoopy95 <joebart95@free.fr>
2021-10-28 11:59:08 +0200

    Up

--- migrations/Version20211028091252.php
+++ migrations/Version20211028091252.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211019114413 extends AbstractMigration
+final class Version20211028091252 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20211019114413 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE dossier ADD step VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE user CHANGE passwordd password VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE dossier DROP step');
         $this->addSql('ALTER TABLE user CHANGE password passwordd VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
